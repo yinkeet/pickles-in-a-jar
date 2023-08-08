a03d468344c582e2118eb2bfd93d42c05bda1f29
yinkeet <wongyinkeet@gmail.com>
2023-08-08 10:23:00 +0800

    Added postgres and adminer to setup initial db table for mails

---
+++ postgres/init.sql
@@ -0,0 +1,12 @@
+CREATE TYPE delivery_status AS ENUM ('pending', 'sending', 'sent');
+
+CREATE TABLE "mails" (
+    "id" SERIAL PRIMARY KEY,
+    "from" VARCHAR(320) NOT NULL,
+    "to" VARCHAR[] NOT NULL,
+    "subject" VARCHAR(988) NOT NULL,
+    "text" TEXT NOT NULL,
+    "status" delivery_status NOT NULL,
+    "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
