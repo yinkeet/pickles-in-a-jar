26427e8779f2ca5e4354d7693d6f209b5800572b
yinkeet <wongyinkeet@gmail.com>
2023-08-08 14:05:12 +0800

    Base folder rename to node There will be a server folder for now and a mailer folder later

--- webhook/app/src/services/logger.ts
+++
@@ -1,9 +0,0 @@
-import winston from "winston";
-
-export const logger = winston.createLogger({
-    level: process.env.LOG_LEVEL || "debug",
-    format: winston.format.json(),
-    transports: [
-        new winston.transports.Console()
-    ],
-});
