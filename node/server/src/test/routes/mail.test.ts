0c08e0e010f13917ad6cf6a72997d18d48b5b645
yinkeet <wongyinkeet@gmail.com>
2023-08-09 10:03:53 +0800

    Added more tests to make sure the validation is implemented correctly

--- node/server/src/test/routes/mail.test.ts
+++ node/server/src/test/routes/mail.test.ts
@@ -12,26 +12,117 @@ jest.mock('./../../services/queue', () => ({
     queueEmail: jest.fn(),
 }));
 
-describe('Post Mail Endpoint', () => {
-    afterAll(async () => {
-        server.close();
-        jest.clearAllMocks();
+describe('Post Mail Endpoint Tests', () => {
+    // Nesting describes inside of describe make sures that we run the silent failure tests in parallel first, then we run the success test
+    // This eliminates some mocks not being cleared in time issue
+    describe('Silent Failures', () => {
+        afterEach(() => {
+            jest.clearAllMocks();
+        });
+        afterAll(async () => {
+            server.close();
+        });
+        test.each([
+            // from field issues
+            [{
+                'to': ['123@gmail.com'],
+                'subject': 'Some subject',
+                'text': 'Important text'
+            }],
+            [{
+                'from': 'abc',
+                'to': ['123@gmail.com'],
+                'subject': 'Some subject',
+                'text': 'Important text'
+            }],
+            [{
+                'from': 123,
+                'to': ['123@gmail.com'],
+                'subject': 'Some subject',
+                'text': 'Important text'
+            }],
+            // to field issues
+            [{
+                'from': 'abc@gmail.com',
+                'subject': 'Some subject',
+                'text': 'Important text'
+            }],
+            [{
+                'from': 'abc@gmail.com',
+                'to': [],
+                'subject': 'Some subject',
+                'text': 'Important text'
+            }],
+            [{
+                'from': 'abc@gmail.com',
+                'to': ['1'],
+                'subject': 'Some subject',
+                'text': 'Important text'
+            }],
+            [{
+                'from': 'abc@gmail.com',
+                'to': [1],
+                'subject': 'Some subject',
+                'text': 'Important text'
+            }],
+            // subject field issues
+            [{
+                'from': 'abc@gmail.com',
+                'to': ['123@gmail.com'],
+                'text': 'Important text'
+            }],
+            [{
+                'from': 'abc',
+                'to': ['123@gmail.com'],
+                'subject': 123,
+                'text': 'Important text'
+            }],
+            // text field issues
+            [{
+                'from': 'abc@gmail.com',
+                'to': ['123@gmail.com'],
+                'subject': 'Some subject'
+            }],
+            [{
+                'from': 'abc',
+                'to': ['123@gmail.com'],
+                'subject': 'Some subject',
+                'text': 123
+            }],
+        ])('should return 204 only (%s)', async (email: Object) => {
+            const response = await request(app).post('/api/v1/email').send(email);
+    
+            expect(response.status).toBe(204);
+            expect(response.text).toBe('');
+            expect(saveEmail).toBeCalledTimes(0);
+            expect(queueEmail).toBeCalledTimes(0);
+        });
     });
-    it('should return 204', async () => {
-        const email: Email = {
-            'from': 'abc@gmail.com',
-            'to': ['123@gmail.com'],
-            'subject': 'Some subject',
-            'text': 'Important text'
-        }
 
-        const response = await request(app).post('/api/v1/email').send(email);
-
-        expect(response.status).toBe(204);
-        expect(response.text).toBe('');
-        expect(saveEmail).toBeCalledTimes(1);
-        expect(saveEmail).toBeCalledWith(email);
-        expect(queueEmail).toBeCalledTimes(1);
-        expect(queueEmail).toBeCalledWith('d94c2eaf-fc21-4e2b-b63c-a8173502238d', email);
+    describe('Success', () => {
+        afterEach(() => {
+            jest.clearAllMocks();
+        });
+        afterAll(async () => {
+            server.close();
+        });
+    
+        it('should return 204 and then save and queue email', async () => {
+            const email: Email = {
+                'from': 'abc@gmail.com',
+                'to': ['123@gmail.com'],
+                'subject': 'Some subject',
+                'text': 'Important text'
+            }
+    
+            const response = await request(app).post('/api/v1/email').send(email);
+    
+            expect(response.status).toBe(204);
+            expect(response.text).toBe('');
+            expect(saveEmail).toBeCalledTimes(1);
+            expect(saveEmail).toBeCalledWith(email);
+            expect(queueEmail).toBeCalledTimes(1);
+            expect(queueEmail).toBeCalledWith('d94c2eaf-fc21-4e2b-b63c-a8173502238d', email);
+        });
     });
-});
+})
