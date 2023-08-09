876dbf72d31653d43d614229ed3601efe389ac2c
yinkeet <wongyinkeet@gmail.com>
2023-08-09 10:03:30 +0800

    Used joi and email validation schema to make sure we have a clean data going into the db

--- node/server/src/routes/mail.ts
+++ node/server/src/routes/mail.ts
@@ -2,7 +2,8 @@ import express, { Request, Response } from 'express';
 import events from 'events';
 import { saveEmail } from './../services/db';
 import { queueEmail } from '../services/queue';
-import { Email } from '../schema/email';
+import { Email, EmailSchema } from '../schema/email';
+import { logger } from '../services/logger';
 
 export const router = express.Router();
 const eventEmitter = new events.EventEmitter();
@@ -14,6 +15,13 @@ router.post('/api/v1/email', (req: Request, res: Response) => {
 });
 
 eventEmitter.on('post_email', async (email: Email) => {
+    // Validate email object and silently exit if an error occurs
+    const { error } = EmailSchema.validate(email)
+    if (error) {
+        return;
+    }
+    // Save the email into db, and use the id generated to queue the email job
+    // The id is later consumed and used to update the db records
     const id = await saveEmail(email);
     await queueEmail(id!, email);
 });
