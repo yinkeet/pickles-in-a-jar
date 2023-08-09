bca3d43b1e45d4bf15ebf8594aa7f19b3d9e361f
yinkeet <wongyinkeet@gmail.com>
2023-08-09 10:03:06 +0800

    Added email validation schema

--- node/server/src/schema/email.ts
+++ node/server/src/schema/email.ts
@@ -1,6 +1,17 @@
+import Joi from 'joi';
+
 export type Email = {
     from: string;
     to: string[];
     subject: string;
     text: string;
 };
+
+export const EmailSchema = Joi.object().keys({
+    from: Joi.string().email().required(),
+    to: Joi.array().min(1).items(
+        Joi.string().email().required()
+    ).required(),
+    subject: Joi.string().required(),
+    text: Joi.string().required()
+});
