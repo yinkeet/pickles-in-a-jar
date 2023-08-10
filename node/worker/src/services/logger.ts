227f762f0085f0d01c345813d277a882b0e44f7c
yinkeet <wongyinkeet@gmail.com>
2023-08-10 16:14:54 +0800

    Added logger

---
+++ node/worker/src/services/logger.ts
@@ -0,0 +1,9 @@
+import winston from "winston";
+
+export const logger = winston.createLogger({
+    level: process.env.LOG_LEVEL || "debug",
+    format: winston.format.json(),
+    transports: [
+        new winston.transports.Console()
+    ],
+});
