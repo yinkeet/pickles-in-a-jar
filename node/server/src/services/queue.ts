31153a623088c1a57cf79ba56e10c7cb1514f6e5
yinkeet <wongyinkeet@gmail.com>
2023-08-11 02:00:21 +0800

    Added websocket code to trigger when mail is being sent and done

--- node/server/src/services/queue.ts
+++ node/server/src/services/queue.ts
@@ -1,6 +1,7 @@
-import Queue from 'bull';
+import Queue, { Job, JobId, JobPromise } from 'bull';
 import { Email } from '../schema/email';
 import { logger } from './logger';
+import WebSocket from 'ws';
 
 const emailQueue = new Queue('email', { 
     redis: { 
@@ -14,3 +15,25 @@ export async function queueEmail(id: string, email: Email) {
     emailQueue.add({ id, ...email });
     logger.debug('Queued');
 };
+
+const wss = new WebSocket.Server({ port: 9001 });
+
+wss.on('connection', (ws) => {
+    emailQueue.on('global:active', async (jobId: JobId, _jobPromise: JobPromise) => {
+        const job = await emailQueue.getJob(jobId);
+        const { id } = job!.data;
+        ws.send(`Sending email: ${id}`);
+    });
+    emailQueue.on('global:completed', async (jobId: JobId, result: string) => {
+        const resultJSON = JSON.parse(result);
+        logger.debug('123');
+        logger.debug(resultJSON);
+        const job = await emailQueue.getJob(jobId);
+        const { id } = job!.data;
+        let msg = `Sent email: ${id}`;
+        if (process.env.NODE_ENV === 'development') {
+            msg += `, Preview URL: ${resultJSON['preview_url']}`
+        }
+        ws.send(msg);
+    })
+});
