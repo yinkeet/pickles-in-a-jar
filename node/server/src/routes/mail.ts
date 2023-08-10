59186c2faf67f570816da2e6707f5e78415734fa
yinkeet <wongyinkeet@gmail.com>
2023-08-11 00:00:59 +0800

    Added routes to get emails

--- node/server/src/routes/mail.ts
+++ node/server/src/routes/mail.ts
@@ -4,6 +4,8 @@ import { saveEmail } from './../services/db';
 import { queueEmail } from '../services/queue';
 import { Email, EmailSchema } from '../schema/email';
 import { logger } from '../services/logger';
+import { createDbConnection } from './../services/db';
+import util from 'util';
 
 export const router = express.Router();
 const eventEmitter = new events.EventEmitter();
@@ -24,4 +26,22 @@ eventEmitter.on('post_email', async (email: Email) => {
     // The id is later consumed and used to update the db records
     const id = await saveEmail(email);
     await queueEmail(id!, email);
+});
+
+router.get('/api/v1/emails', async (req: Request, res: Response) => {
+    const client = await createDbConnection();
+    try {
+        const { rows: results } = await client.query(`SELECT * FROM mails`)
+        return res.json({
+            'results': results
+        })
+    } catch (e) {
+        const error = util.inspect(e);
+        logger.error(error)
+        return res.status(500).json({
+            error
+        })
+    } finally {
+        client.release();
+    }
 });
